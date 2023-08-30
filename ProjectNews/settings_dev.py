5fa5ef03be48afa45c7e001cb785abf3ff2464c2
Lskram <dankflamemasterLove@gmail.com>
2023-08-30 23:29:25 +0700

    update

--- ProjectNews/settings_dev.py
+++ ProjectNews/settings_dev.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'ProjectNews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':   'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'1234',
+        'HOST':'db.gdlhnbqsvbekczxakuar.supabase.co',
+        'PORT':'5432'
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
