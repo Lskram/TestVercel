d286ff79f37daa48b1367be221c564b269071b57
Lskram <dankflamemasterLove@gmail.com>
2023-08-31 11:52:38 +0700

    statics

--- ProjectNews/settings.py
+++ ProjectNews/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS =[Path.joinpath(BASE_DIR,'static')]
-STATIC_ROOT =Path.joinpath(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'statics', ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
