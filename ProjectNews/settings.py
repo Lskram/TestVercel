66926b307eb4908487236b7d7a1202efd8fadb21
Lskram <dankflamemasterLove@gmail.com>
2023-08-31 10:11:31 +0700

    555

--- ProjectNews/settings.py
+++ ProjectNews/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'News',
+    'django_dump_load_utf8',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'ProjectNews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':   'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'Aeiou123aZ555',
+        'HOST':'db.gdlhnbqsvbekczxakuar.supabase.co',
+        'PORT':'5432'
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- ProjectNews/settings_dev.py
+++ ProjectNews/settings_dev.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'News',
+    'django_dump_load_utf8',
 ]
 
 MIDDLEWARE = [
@@ -74,17 +75,13 @@ WSGI_APPLICATION = 'ProjectNews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':   'postgres',
-        'USER': 'postgres',
-        'PASSWORD':'1234',
-        'HOST':'db.gdlhnbqsvbekczxakuar.supabase.co',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
