97d52d933d2ecd6d09b65557291e8c8275833287
Lskram <dankflamemasterLove@gmail.com>
2023-09-07 10:21:17 +0700

    ตรวจทานความถูกต้องเเล้ว

--- ProjectNews/settings.py
+++ ProjectNews/settings.py
@@ -127,9 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
